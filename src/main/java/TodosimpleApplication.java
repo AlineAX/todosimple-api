7068d46ac3ac76859cc724cb069462ad65d92c81
Aline Xavier <alineaxavier@icloud.com>
2024-06-25 18:07:44 -0300

    Configure project

--- src/main/java/TodosimpleApplication.java
+++ src/main/java/TodosimpleApplication.java
@@ -1,9 +1,10 @@
-package com.alinexavier.todosimple;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class TodosimpleApplication {
 
 	public static void main(String[] args) {
