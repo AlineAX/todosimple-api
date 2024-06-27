42c4206668fcf8e0e97abf2bed53fa7bc9a21c87
Aline Xavier <alineaxavier@icloud.com>
2024-06-27 16:40:03 -0300

    ajustes para funcionar

--- src/main/java/com/todosimple/models/User.java
+++ src/main/java/com/todosimple/models/User.java
@@ -2,18 +2,23 @@ package com.todosimple.models;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import jakarta.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "user")
+@Table(name = User.TABLE_NAME)
 public class User {
     public interface CreateUser {
     }
@@ -34,11 +39,16 @@ public class User {
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
-    @JsonProperty(access = Acess.WRITE_ONLY)
-    @Column(name="password",length=60,nullable=false)@NotNull(groups={CreateUser.class,UpdateUser.class})@NotEmpty(groups={CreateUser.class,UpdateUser.class})@Size(groups=C{CreateUser.class, UpdateUser.class},min=8,max=60)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List <Task>tasks = new ArrayList<Task>();
+
+    @OneToMany(mappedBy = "user")
+    private List <Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -78,35 +88,48 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-            if (obj == null) {
-                return false;
-            }
-        if (!(obj instaceof User)){
-            return false;
-        } 
-        User other  = (User) obj;
-        if (this.id == null) {
-            if (other.id != null) 
-                return false;
-                else if (!this.id.equals(obj.id)) 
-                    return false;
-                    return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
-                    && Objects.equals(this.password, other.password);
-                
-            
-        }
-
-    @Override
     public int hashCode() {
-        final int prime = 31; 
+        final int prime = 31;
         int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
         return result;
     }
 
-}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    
 }
