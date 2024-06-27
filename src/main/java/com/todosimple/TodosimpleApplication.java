ff5f0cd7add0109641f6ac401a93caf528d11116
Aline Xavier <alineaxavier@icloud.com>
2024-06-27 16:42:04 -0300

    teste

--- src/main/java/com/todosimple/TodosimpleApplication.java
+++ src/main/java/com/todosimple/TodosimpleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodosimpleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MEU PROJETO");
 		SpringApplication.run(TodosimpleApplication.class, args);
 	}
 
